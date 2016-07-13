68aa47a22d79ef3514c06455ccec9db029f788b7
Matt Lunalover <mlunalover@kabbage.com>
2016-07-13 09:47:11 -0400

    setup

---
+++ recipe-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "./header.component";
+
+@Component({
+  moduleId: module.id,
+  selector: 'recipe-book-app',
+  templateUrl: 'recipe-book.component.html',
+  directives: [HeaderComponent]
+})
+export class RecipeBookAppComponent {
+
+}
