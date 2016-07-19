f10a6ce4955067b477cd8e2bade6a072bbeebf97
Matt Lunalover <mlunalover@kabbage.com>
2016-07-19 16:48:29 -0400

    added dropdown directive

--- recipes/recipe-list/recipe-list.component.ts
+++ recipes/recipe-list/recipe-list.component.ts
@@ -10,9 +10,11 @@ import { RecipeItemComponent } from './recipe-item.component';
   directives: [RecipeItemComponent]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Shnitzel', 'Very tasty', 'http://cook-wise.com/wp-content/uploads/parser/recipe-book-4.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://cook-wise.com/wp-content/uploads/parser/recipe-book-4.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://cook-wise.com/wp-content/uploads/parser/recipe-book-4.jpg');
 
   constructor() {}
 
