0353d67e59aa50abdc10218a8b2709041bd821c3
Matt Lunalover <mlunalover@kabbage.com>
2016-07-19 16:17:56 -0400

    more fixes

--- recipe-book.component.ts
+++ recipe-book.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header.component";
 import {RecipesComponent} from "./recipes";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 
 @Component({
   moduleId: module.id,
   selector: 'recipe-book-app',
   templateUrl: 'recipe-book.component.html',
-  directives: [HeaderComponent, RecipesComponent]
+  directives: [HeaderComponent, RecipesComponent, ShoppingListComponent]
 })
 export class RecipeBookAppComponent {
 
